69d92c9ed6b8796b3733565bb6f853d342114cef
ocelot-browser <lachlanh@protonmail.ch>
2016-05-21 11:55:04 +1000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <QWidget>
+#include <QApplication>
+#include <QtWebEngineWidgets>
+#include <QVBoxLayout>
+#include <QShortcut>
+#include <QObject>
+#include <QKeySequence>
+
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  QWidget window;
+  QUrl url;
+
+
+  QWebEngineView *view = new QWebEngineView();
+  window.setWindowFlags( Qt::CustomizeWindowHint );
+  window.setWindowTitle("Browser");
+  window.showMaximized();
+
+  url = QUrl("file:///home/ospf/Programming/ocelotbrowser/Home/ocelot.html");
+  view->load(url);
+  view->show();
+
+  QVBoxLayout *vbox = new QVBoxLayout();
+  vbox->addWidget(view);
+  vbox->setMargin(0);
+  window.setLayout(vbox);
+  window.show();
+
+  QShortcut *reload = new QShortcut(QKeySequence("Ctrl+k"), view);
+  QObject::connect(reload, SIGNAL(activated()), view, SLOT(reload()));
+
+  QShortcut *back = new QShortcut(QKeySequence("Ctrl+h"), view);
+  QObject::connect(back, SIGNAL(activated()), view, SLOT(back()));
+
+  QShortcut *min = new QShortcut(QKeySequence("Ctrl+j"), view);
+  QObject::connect(min, SIGNAL(activated()), view, SLOT(showMinimized()));
+
+  QShortcut *forward = new QShortcut(QKeySequence("Ctrl+l"), view);
+  QObject::connect(forward, SIGNAL(activated()), view, SLOT(forward()));
+
+
+  QShortcut *home = new QShortcut(QKeySequence("Ctrl+w"), view);
+  QObject::connect(home, SIGNAL(activated()), view, SLOT(home()));
+
+    //QShortcut *speed_dial1 = new QShortcut(QKeySequence("Ctrl+a"), view);
+    //QObject::connect(speed_dial1, SIGNAL(activated()), view, SLOT(load("http://qt-project.org/support/file.html")));
+
+
+
+
+return app.exec();
+
+ }
+
+
+
+
---
+++ ocelotbrowser.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-09-24T14:37:33
+#
+#-------------------------------------------------
+
+QT       += core gui widgets webenginewidgets
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ocelotbrowser
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS  +=
+
+FORMS    +=
+
+DISTFILES += \
+    browser.qml
